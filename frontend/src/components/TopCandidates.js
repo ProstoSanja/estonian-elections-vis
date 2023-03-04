a8d2320f0d49dc0fa24f31c171653e371bc713b3
Aleksandr Tsernoh <aleksandr@salv.com>
2023-03-04 21:10:10 +0200

    show more button

--- frontend/src/components/TopCandidates.js
+++ frontend/src/components/TopCandidates.js
@@ -1,13 +1,17 @@
 import PersonCard from "./PersonCard";
+import {useState} from "react";
 
 function TopCandidates({
   candidates
 }) {
+  const [showNumber, setShowNumber] = useState(49);
+
   return (
     <div className="TopCandidates">
-      {candidates.length > 0 && candidates.slice(0, 50).map((candidate) => {
+      {candidates.length > 0 && candidates.slice(0, showNumber).map((candidate) => {
         return <PersonCard key={candidate.regNumber} candidate={candidate}/>
       })}
+      <button onClick={() => {setShowNumber(showNumber+25);}}>Näita veel</button>
     </div>
   );
 }
