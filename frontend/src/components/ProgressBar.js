c99d2a2b74a5cbde66ee414e9729e5b1f630cc5a
Aleksandr Tsernoh <aleksandr@salv.com>
2023-03-05 11:53:39 +0200

    evotes indicator

--- frontend/src/components/ProgressBar.js
+++ frontend/src/components/ProgressBar.js
@@ -16,8 +16,8 @@ function ProgressBar({
       <h3>Häälte lugemine</h3>
       <span>Kokku loetud: {donePercent.toFixed(2)}%</span>
       <span>Protokolle esitatud: {stats.protocolsCounted}/{stats.protocolsTotal}</span>
-      <br/>
       <div className="ProgressBarHolder">
+        <div style={{background: stats.evotesCounted ? "lightcyan" : "deepskyblue"}} className="progressBarEVotes"/>
         <div style={{width:donePercent + "%"}} className="progressBarCounted"/>
         <div style={{width:notDonePercent + "%"}}  className="progressBarNotCounted"/>
       </div>
