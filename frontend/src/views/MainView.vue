8f3c56918cd90120225cf55361f07258b0ae3584
Alex Tsernoh <alex@thatguyalex.com>
2025-10-15 22:33:08 +0300

    scrollbar

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -38,8 +38,8 @@ const globalVoteCountingStarter = computed<boolean>(() => {
 })
 </script>
 <template>
-  <div class="flex flex-col md:flex-row w-screen  min-w-screen max-w-screen md:h-screen min-h-screen md:max-h-screen bg-slate-800 text-white md:overflow-hidden">
-    <div class="flex flex-col w-full md:w-1/2 lg:w-3/5 p-4 lg:p-8 gap-4 md:in-h-screen items-center md:overflow-y-auto overflow-x-hidden order-2 md:order-1">
+  <div class="flex flex-col md:flex-row w-screen  min-w-screen max-w-screen md:h-screen min-h-screen md:max-h-screen bg-slate-800 text-white md:overflow-hidden scrollbar-thumb-slate-900  scrollbar-track-slate-800 scrollbar-w-2">
+    <div class="flex flex-col w-full md:w-1/2 lg:w-3/5 p-4 lg:p-8 gap-4 md:in-h-screen items-center md:overflow-y-auto overflow-x-hidden order-2 md:order-1 md:scrollbar-thin">
       <h1 class="text-6xl font-bold hidden md:block">Eesti {{ electionTitle.type }} {{ electionTitle.year }} valimisõhtu</h1>
       <span class="text-xl" v-if="!globalVoteCountingStarter">Ootame esimesi hääli valimispäeval alates kella
         20:00</span>
@@ -47,7 +47,7 @@ const globalVoteCountingStarter = computed<boolean>(() => {
       <EestiCandidates />
       <FooterView />
     </div>
-    <div class="flex flex-col w-full md:w-1/2 lg:w-2/5 p-4 lg:p-8 gap-4 md:in-h-screen items-center md:overflow-y-auto overflow-x-hidden md:border-l-2 order-1 md:order-2 md:border-slate-900">
+    <div class="flex flex-col w-full md:w-1/2 lg:w-2/5 p-4 lg:p-8 gap-4 md:in-h-screen items-center md:overflow-y-auto overflow-x-hidden md:border-l-2 order-1 md:order-2 md:border-slate-900 md:scrollbar-thin">
       <h1 class="text-5xl font-bold block md:hidden py-4">Eesti {{ electionTitle.type }} {{ electionTitle.year }} valimisõhtu</h1>
       <EestiDashbaord />
     </div>
